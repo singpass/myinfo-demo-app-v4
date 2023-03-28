af987e60af85e8c5581cc79aa7516f3dc06b472a
Cheong Kahou <kahou.cheong@accenture.com>
2023-03-28 11:53:35 +0800

    refactor code to use node connector on npm

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-const cors = require('cors');
-const colors = require('colors');
-const crypto = require('crypto');
-var MyInfoConnector = require('./myinfo-connector/index.js');
+const express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+const cors = require("cors");
+const colors = require("colors");
+const crypto = require("crypto");
+var MyInfoConnector = require("myinfo-connector-v4-nodejs");
+const fs = require("fs");
 
 const app = express();
 const port = 3001;
-const config = require('./config/config.js');
+const config = require("./config/config.js");
 const connector = new MyInfoConnector(config.MYINFO_CONNECTOR_CONFIG);
 
 var sessionIdCache = {};
@@ -17,109 +18,166 @@ var sessionIdCache = {};
 app.use(express.json());
 app.use(cors());
 
-app.set('views', path.join(__dirname, 'public/views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "public/views"));
+app.set("view engine", "pug");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + `/public/index.html`);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + `/public/index.html`);
 });
 
 // get the environment variables (app info) from the config
-app.get('/getEnv', function (req, res) {
-
-    try {
-        if (config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined || config.APP_CONFIG.DEMO_APP_CLIENT_ID == null) {
-            res.status(500).send({
-                "error": "Missing Client ID"
-            });
-        } else {
-            res.status(200).send({
-                "clientId": config.APP_CONFIG.DEMO_APP_CLIENT_ID,
-                "redirectUrl": config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
-                "scope": config.APP_CONFIG.DEMO_APP_SCOPES,
-                "purpose_id": config.APP_CONFIG.DEMO_APP_PURPOSE_ID,
-                "authApiUrl": config.APP_CONFIG.MYINFO_API_AUTHORIZE,
-            });
-        }
-    } catch (error) {
-        console.log("Error".red, error);
-        res.status(500).send({
-            "error": error
-        });
+app.get("/getEnv", function (req, res) {
+  try {
+    if (
+      config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined ||
+      config.APP_CONFIG.DEMO_APP_CLIENT_ID == null
+    ) {
+      res.status(500).send({
+        error: "Missing Client ID",
+      });
+    } else {
+      res.status(200).send({
+        clientId: config.APP_CONFIG.DEMO_APP_CLIENT_ID,
+        redirectUrl: config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
+        scope: config.APP_CONFIG.DEMO_APP_SCOPES,
+        purpose_id: config.APP_CONFIG.DEMO_APP_PURPOSE_ID,
+        authApiUrl: config.APP_CONFIG.MYINFO_API_AUTHORIZE,
+        subentity: config.APP_CONFIG.DEMO_APP_SUBENTITY_ID,
+      });
     }
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      error: error,
+    });
+  }
 });
 
 // callback function - directs back to home page
-app.get('/callback', function (req, res) {
-    res.sendFile(__dirname + `/public/index.html`);
+app.get("/callback", function (req, res) {
+  res.sendFile(__dirname + `/public/index.html`);
 });
 
-// getPersonData function - call MyInfo Token + Person API
-app.post('/getPersonData', async function (req, res, next) {
-    try {
-        // get variables from frontend
-        var authCode = req.body.authCode;
-        var codeVerifier = sessionIdCache[req.cookies.sid];
-        console.log("Calling MyInfo NodeJs Library...".green);
-
-        let personData = await connector.getMyInfoPersonData(authCode, codeVerifier);
-
-        /* 
-        P/s: Your logic to handle the person data ...
-        */
-        console.log('--- Sending Person Data From Your-Server (Backend) to Your-Client (Frontend)---:'.green);
-        console.log(JSON.stringify(personData)); // log the data for demonstration purpose only
-        res.status(200).send(personData); //return personData
-
-    } catch (error) {
-        console.log("---MyInfo NodeJs Library Error---".red);
-        console.log(error);
-        res.status(500).send({
-            "error": error
-        });
+//function to read multiple files from a directory
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function (err, filenames) {
+    if (err) {
+      onError(err);
+      return;
     }
+    filenames.forEach(function (filename) {
+      fs.readFile(dirname + filename, "utf8", function (err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+// getPersonData function - call MyInfo Token + Person API
+app.post("/getPersonData", async function (req, res, next) {
+  try {
+    // get variables from frontend
+    var authCode = req.body.authCode;
+    //retrieve code verifier from session cache
+    var codeVerifier = sessionIdCache[req.cookies.sid];
+    console.log("Calling MyInfo NodeJs Library...".green);
+
+    // retrieve private siging key and decode to utf8 from FS
+    let privateSigningKey = fs.readFileSync(
+      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
+      "utf8"
+    );
+
+    let privateEncryptionKeys = [];
+    // retrieve private encryption keys and decode to utf8 from FS, insert all keys to array
+    readFiles(
+      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS,
+      (filename, content) => {
+        privateEncryptionKeys.push(content);
+      },
+      (err) => {
+        throw err;
+      }
+    );
+
+    //call myinfo connector to retrieve data
+    let personData = await connector.getMyInfoPersonData(
+      authCode,
+      codeVerifier,
+      privateSigningKey,
+      privateEncryptionKeys
+    );
+
+    /* 
+      P/s: Your logic to handle the person data ...
+    */
+    console.log(
+      "--- Sending Person Data From Your-Server (Backend) to Your-Client (Frontend)---:"
+        .green
+    );
+    console.log(JSON.stringify(personData)); // log the data for demonstration purpose only
+    res.status(200).send(personData); //return personData
+  } catch (error) {
+    console.log("---MyInfo NodeJs Library Error---".red);
+    console.log(error);
+    res.status(500).send({
+      error: error,
+    });
+  }
 });
 
 // Generate the code verifier and code challenge for PKCE flow
-app.post('/generateCodeChallenge', async function (req, res, next) {
-    try {
-        let pkceCodePair = connector.generatePKCECodePair();
-        let sessionId = crypto.randomBytes(16).toString('hex');
-        sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
+app.post("/generateCodeChallenge", async function (req, res, next) {
+  try {
+    // call connector to generate code_challenge and code_verifier
+    let pkceCodePair = connector.generatePKCECodePair();
+    // create a session and store code_challenge and code_verifier pair
+    let sessionId = crypto.randomBytes(16).toString("hex");
+    sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
 
-        res.cookie("sid", sessionId);
-        res.status(200).send(pkceCodePair.codeChallenge);
-    } catch (error) {
-        console.log("Error".red, error);
-        res.status(500).send({
-            "error": error
-        });
-    }
+    //establish a frontend session with browser to retrieve back code_verifier
+    res.cookie("sid", sessionId);
+    //send code code_challenge to frontend to make /authorize call
+    res.status(200).send(pkceCodePair.codeChallenge);
+  } catch (error) {
+    console.log("Error".red, error);
+    res.status(500).send({
+      error: error,
+    });
+  }
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 // print stacktrace on error
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: err
-    });
+  res.status(err.status || 500);
+  res.render("error", {
+    message: err.message,
+    error: err,
+  });
 });
 
-app.listen(port, () => console.log(`Demo App Client listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Demo App Client listening on port ${port}!`)
+);
--- config/config.js
+++ config/config.js
@@ -2,77 +2,91 @@
 /**
  * Myinfo Demo App supports the following environments:
  * 
- * SANDBOX: 
- * Without Encryption and Signing
- * Note: The sandbox environment is used for your testing when developing your prototype
- * 
  * TEST:
  * With Encryption and Signing 
  * Note: The test environment is used for testing your application with the full security measures required in production
  */
-let ENVIRONMENT = process.argv[2];
 
+let ENVIRONMENT = process.argv[2];
+let urlEnvironmentPrefix = ENVIRONMENT == "prod" ? "" : `${ENVIRONMENT}.`;
+
+/**
+ * Set the following demo app configurations for the demo app to run
+ * 
+ * DEMO_APP_CLIENT_ID: Client id provided during onboarding
+ * DEMO_APP_SUBENTITY_ID: optional parameter for platform applications only
+ * DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY : private signing key for client_assertion
+ * DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS : folder to private encryption keys, allow multiple keys to match multiple encryption keys in JWKS
+ * DEMO_APP_PURPOSE_ID: purpose_id with reference to purpose that will be shown to user on consent page provided during onboarding
+ * DEMO_APP_SCOPES: Space separated list of attributes to be retrieved from Myinfo
+ * MYINFO_API_AUTHORIZE: The URL for Authorize API
+ */
 let APP_CONFIG = {
-  'DEMO_APP_CLIENT_ID': 'STG2-MYINFO-SELF-TEST', //need to update to demo app for roll out
-  'DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY': './cert/your-sample-app-signing-private-key.pem',
-  'DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEY': './cert/your-sample-app-encryption-private-key.pem',
-  'DEMO_APP_CALLBACK_URL': 'http://localhost:3001/callback',
-  'DEMO_APP_PURPOSE_ID': 'demonstration',
-  'DEMO_APP_SCOPES': 'uinfin name sex race nationality dob email mobileno regadd housingtype hdbtype marital edulevel noa-basic ownerprivate cpfcontributions cpfbalances',
-  'MYINFO_API_AUTHORIZE': `https://${ENVIRONMENT}.api.myinfo.gov.sg/com/v4/authorize`
-}
+  // DEMO_APP_CLIENT_ID: "STG2-MYINFO-DEMO-APP", 
+  DEMO_APP_CLIENT_ID: "STG2-MYINFO-SELF-TEST", 
+  DEMO_APP_SUBENTITY_ID: "", //only for platform apps
+  DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY: "./cert/your-sample-app-signing-private-key.pem",
+  DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS: "./cert/encryption-private-keys/",
+  DEMO_APP_CALLBACK_URL: "http://localhost:3001/callback",
+  DEMO_APP_PURPOSE_ID: "demonstration",
+  DEMO_APP_SCOPES : "uinfin name sex race nationality dob email mobileno regadd housingtype hdbtype marital edulevel noa-basic ownerprivate cpfcontributions cpfbalances",
+  MYINFO_API_AUTHORIZE: `https://${urlEnvironmentPrefix}api.myinfo.gov.sg/com/v4/authorize`
+};
 
 
 /**
  * Set following configuration for MyInfo library to call token and person API
  * Based on the environment, the corresponding config will be used.
  * IMPORTANT: DO NOT rename the JSON Keys
- * 
+ *
  * Myinfo Connector config has the following mandatory parameters:
- * 
+ *
  * CLIENT_ID: Client id provided during onboarding
- * CLIENT_SECURE_CERT: Alias of the application private key in P12 format
- * CLIENT_SECURE_CERT_PASSPHRASE: Password of the private key
  * REDIRECT_URL: Redirect URL for web application
  * SCOPE: Space separated list of attributes to be retrieved from Myinfo
  * AUTHORIZE_JWKS_URL: The URL to retrieve authorize JWKS public key
  * MYINFO_JWKS_URL: The URL to retrieve Myinfo JWKS public key
  * TOKEN_URL: The URL for Token API
  * PERSON_URL: The URL for Person API
- * ENABLE_JWE: Determines whether encryption is used in the application. Values accepted: Y or N. 
- * ENABLE_JWS: Determines whether signing is used in the application. Values accepted: Y or N. 
+ *
+ * Optional parameters
+ * CLIENT_ASSERTION_SIGNING_KID : kid that will be appended to client_assertion header to match JWKS kid
+ * SUBENTITY_ID: optional parameter for platform applications only
  * 
  * Proxy parameters are optional:
  * USE_PROXY: Indicate whether proxy url is used. Values accepted: Y or N
  * PROXY_TOKEN_URL: Configure your proxy url here, if any.
  * PROXY_PERSON_URL: Configure your proxy url here, if any.
- * 
- * Miscellaneous parameters: 
+ *
+ * Miscellaneous parameters:
  * DEBUG_LEVEL
- * 
+ *
  * Debug level for library logging. i.e 'error, info, debug' leave empty to turn off logs (OPTIONAL)
  * error - Log out all the errors returned from the library
  * info - log urls called, authorization headers and errors from the library
- * debug - Full logs from the library, i.e (errors, urls, authorization headers, API response) 
+ * debug - Full logs from the library, i.e (errors, urls, authorization headers, API response)
  * NOTE: debug mode should never be turned on in production
  */
+
 let MYINFO_CONNECTOR_CONFIG = {
-  'CLIENT_ID': APP_CONFIG.DEMO_APP_CLIENT_ID,
-  'CLIENT_PRIVATE_SIGNING_KEY': APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
-  'CLIENT_PRIVATE_ENCRYPTION_KEY': APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEY,
-  'REDIRECT_URL': APP_CONFIG.DEMO_APP_CALLBACK_URL,
-  'SCOPE': APP_CONFIG.DEMO_APP_SCOPES,
-  'AUTHORIZE_JWKS_URL': `https://test.authorise.singpass.gov.sg/.well-known/keys.json`,
-  'MYINFO_JWKS_URL': `https://test.authorise.singpass.gov.sg/.well-known/keys.json`,
-  'TOKEN_URL': `https://${ENVIRONMENT}.api.myinfo.gov.sg/com/v4/token`,
-  'PERSON_URL': `https://${ENVIRONMENT}.api.myinfo.gov.sg/com/v4/person`,
-  'ENABLE_JWE': ENVIRONMENT.toUpperCase() == "SANDBOX" ? 'N' : 'Y',
-  'ENABLE_JWS': ENVIRONMENT.toUpperCase() == "SANDBOX" ? 'N' : 'Y',
-  'USE_PROXY': 'N',
-  'PROXY_TOKEN_URL': '',
-  'PROXY_PERSON_URL': '',
-  'DEBUG_LEVEL': 'info'
-}
+  CLIENT_ID: APP_CONFIG.DEMO_APP_CLIENT_ID,
+  SUBENTITY_ID: APP_CONFIG.DEMO_APP_SUBENTITY_ID,
+  REDIRECT_URL: APP_CONFIG.DEMO_APP_CALLBACK_URL,
+  SCOPE : APP_CONFIG.DEMO_APP_SCOPES,
+  AUTHORIZE_JWKS_URL: `https://${urlEnvironmentPrefix}authorise.singpass.gov.sg/.well-known/keys.json`,
+  MYINFO_JWKS_URL: `https://${urlEnvironmentPrefix}authorise.singpass.gov.sg/.well-known/keys.json`,
+  TOKEN_URL: `https://${urlEnvironmentPrefix}api.myinfo.gov.sg/com/v4/token`,
+  PERSON_URL: `https://${urlEnvironmentPrefix}api.myinfo.gov.sg/com/v4/person`,
+  CLIENT_ASSERTION_SIGNING_KID :'', // optional parameter to specify specific kid for signing. Default will be thumbprint of JWK
+  USE_PROXY: "N",
+  PROXY_TOKEN_URL: "",
+  PROXY_PERSON_URL: "",
+  DEBUG_LEVEL: "info"
+};
+
+
+console.log("MYINFO_CONNECTOR_CONFIG", MYINFO_CONNECTOR_CONFIG);
+
 
 module.exports.APP_CONFIG = APP_CONFIG;
 module.exports.MYINFO_CONNECTOR_CONFIG = MYINFO_CONNECTOR_CONFIG;
