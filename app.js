88045ad63b145207db90c023e08e8a10405111c2
YahyaMH22 <ext_yahya.s.m.khamayseh@malaysiaairlines.com>
2023-05-29 12:20:39 +0300

    refactor

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
 const cors = require("cors");
-const colors = require("colors");
 const crypto = require("crypto");
 var MyInfoConnector = require("myinfo-connector-v4-nodejs");
 const fs = require("fs");
@@ -13,9 +12,9 @@ const app = express();
 const port = 3001;
 const config = require("./config/config.js");
 const connector = new MyInfoConnector(config.MYINFO_CONNECTOR_CONFIG);
-const { v4: uuidv4 } = require('uuid');
 
 var sessionIdCache = {};
+let tadabaseRedirectURL;
 
 app.use(express.json());
 app.use(cors());
@@ -32,10 +31,6 @@ app.use(
   })
 );
 app.use(cookieParser());
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + `/public/index.html`);
-});
 
 app.get("/jwks", async function (req, res) {
   const keysPath = __dirname + "/keys.json";
@@ -69,37 +64,95 @@ app.get("/jwks", async function (req, res) {
   res.send(keyStore.toJSON());
 });
 
-// get the environment variables (app info) from the config
-app.get("/getEnv", function (req, res) {
-  try {
-    if (
-      config.APP_CONFIG.DEMO_APP_CLIENT_ID == undefined ||
-      config.APP_CONFIG.DEMO_APP_CLIENT_ID == null
-    ) {
-      res.status(500).send({
-        error: "Missing Client ID",
-      });
-    } else {
-      res.status(200).send({
-        clientId: config.APP_CONFIG.DEMO_APP_CLIENT_ID,
-        redirectUrl: config.APP_CONFIG.DEMO_APP_CALLBACK_URL,
-        scope: config.APP_CONFIG.DEMO_APP_SCOPES,
-        purpose_id: config.APP_CONFIG.DEMO_APP_PURPOSE_ID,
-        authApiUrl: config.APP_CONFIG.MYINFO_API_AUTHORIZE,
-        subentity: config.APP_CONFIG.DEMO_APP_SUBENTITY_ID,
-      });
-    }
-  } catch (error) {
-    console.log("Error".red, error);
-    res.status(500).send({
-      error: error,
-    });
-  }
+app.get("/login", (req, res) => {
+  tadabaseRedirectURL = req.query.tadabaseRedirectURL;
+
+  const clientId = config.APP_CONFIG.DEMO_APP_CLIENT_ID;
+  const redirectUrl = config.APP_CONFIG.DEMO_APP_CALLBACK_URL;
+  const scope = config.APP_CONFIG.DEMO_APP_SCOPES;
+  const purposeId = config.APP_CONFIG.DEMO_APP_PURPOSE_ID;
+  const authApiUrl = config.APP_CONFIG.MYINFO_API_AUTHORIZE;
+  const subentity = config.APP_CONFIG.DEMO_APP_SUBENTITY_ID;
+
+  const method = "S256";
+  const clientAssertionType =
+    "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
+
+  let securityEnable;
+
+  // call connector to generate code_challenge and code_verifier
+  let pkceCodePair = connector.generatePKCECodePair();
+  // create a session and store code_challenge and code_verifier pair
+  let sessionId = crypto.randomBytes(16).toString("hex");
+  sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
+  // codeVerifier = pkceCodePair.codeVerifier;
+
+  //establish a frontend session with browser to retrieve back code_verifier
+  res.cookie("sid", sessionId);
+  //send code code_challenge to frontend to make /authorize call
+  const codeChallenge = pkceCodePair.codeChallenge;
+
+  const authorizeUrl =
+    authApiUrl +
+    "?client_id=" +
+    clientId +
+    "&scope=" +
+    scope +
+    "&purpose_id=" +
+    purposeId +
+    "&code_challenge=" +
+    codeChallenge +
+    "&code_challenge_method=" +
+    method +
+    "&redirect_uri=" +
+    redirectUrl;
+
+  res.redirect(authorizeUrl);
 });
 
 // callback function - directs back to home page
-app.get("/callback", function (req, res) {
-  res.sendFile(__dirname + `/public/index.html`);
+app.get("/callback", async function (req, res) {
+  const authCode = req.query.code;
+  //retrieve code verifier from session cache
+  const codeVerifier = sessionIdCache[req.cookies.sid];
+  console.log("Calling MyInfo NodeJs Library...".green);
+
+  // retrieve private siging key and decode to utf8 from FS
+  let privateSigningKey = fs.readFileSync(
+    config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
+    "utf8"
+  );
+
+  let privateEncryptionKeys = [];
+  // retrieve private encryption keys and decode to utf8 from FS, insert all keys to array
+  console.log(
+    "config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS",
+    config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS
+  );
+  readFiles(
+    config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS,
+    (filename, content) => {
+      privateEncryptionKeys.push(content);
+    },
+    (err) => {
+      throw err;
+    }
+  );
+
+  console.log("authCode", authCode);
+  console.log("codeVerifier", codeVerifier);
+  console.log("privateSigningKey", privateSigningKey);
+  console.log("privateEncryptionKeys", privateEncryptionKeys);
+
+  // call myinfo connector to retrieve data
+  let personData = await connector.getMyInfoPersonData(
+    authCode,
+    codeVerifier,
+    privateSigningKey,
+    privateEncryptionKeys
+  );
+
+  console.log("personData", personData);
 });
 
 //function to read multiple files from a directory
@@ -120,127 +173,6 @@ function readFiles(dirname, onFileContent, onError) {
     });
   });
 }
-
-// getPersonData function - call MyInfo Token + Person API
-app.post("/getPersonData", async function (req, res, next) {
-  try {
-    // get variables from frontend
-    const authCode = req.body.authCode;
-    //retrieve code verifier from session cache
-    const codeVerifier = sessionIdCache[req.cookies.sid];
-    console.log("Calling MyInfo NodeJs Library...".green);
-
-    // retrieve private siging key and decode to utf8 from FS
-    let privateSigningKey = fs.readFileSync(
-      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_SIGNING_KEY,
-      "utf8"
-    );
-
-    let privateEncryptionKeys = [];
-    // retrieve private encryption keys and decode to utf8 from FS, insert all keys to array
-    readFiles(
-      config.APP_CONFIG.DEMO_APP_CLIENT_PRIVATE_ENCRYPTION_KEYS,
-      (filename, content) => {
-        privateEncryptionKeys.push(content);
-      },
-      (err) => {
-        throw err;
-      }
-    );
-
-    // call myinfo connector to retrieve data
-    let personData = await connector.getMyInfoPersonData(
-      authCode,
-      codeVerifier,
-      privateSigningKey,
-      privateEncryptionKeys
-    );
-
-    /* 
-      P/s: Your logic to handle the person data ...
-    */
-    console.log(
-      "--- Sending Person Data From Your-Server (Backend) to Your-Client (Frontend)---:"
-        .green
-    );
-    console.log(JSON.stringify(personData)); // log the data for demonstration purpose only
-    res.status(200).send(personData); //return personData
-  } catch (error) {
-    console.log("---MyInfo NodeJs Library Error---".red);
-    console.log(error);
-    res.status(500).send({
-      error: error,
-    });
-  }
-});
-
-async function verifyToken(context, token, keyStore) {
-  if (!token) return null;
-
-  try {
-    const singpassJwksURL =
-      process.env.SINGPASS_API_PREFIX + "/.well-known/keys";
-    const jwksResponse = await fetch(singpassJwksURL);
-    const jwks = await jwksResponse.json();
-
-    const [key] = keyStore.all({ use: "enc" });
-    const decrypt = jose.JWE.createDecrypt(key);
-    const decrypted = await decrypt.decrypt(token);
-
-    // const kid = decrypted.header.kid;
-    const signKey = jwks.keys.find((key) => key.use == "sig");
-    const signKeyJWK = await jose.JWK.asKey(signKey, "json");
-    const verify = jose.JWS.createVerify(signKeyJWK);
-    const payload = Buffer.from(decrypted.payload, "base64").toString();
-    await verify.verify(payload);
-
-    const components = payload.split(".");
-    const decoded = JSON.parse(base64url.decode(components[1]));
-    context.log("decoded", decoded);
-    return decoded;
-  } catch (error) {
-    context.log(error);
-    return null;
-  }
-}
-
-// Generate the code verifier and code challenge for PKCE flow
-app.post("/generateCodeChallenge", async function (req, res, next) {
-  try {
-    // call connector to generate code_challenge and code_verifier
-    let pkceCodePair = connector.generatePKCECodePair();
-    // create a session and store code_challenge and code_verifier pair
-    let sessionId = crypto.randomBytes(16).toString("hex");
-    sessionIdCache[sessionId] = pkceCodePair.codeVerifier;
-
-    //establish a frontend session with browser to retrieve back code_verifier
-    res.cookie("sid", sessionId);
-    //send code code_challenge to frontend to make /authorize call
-    res.status(200).send(pkceCodePair.codeChallenge);
-  } catch (error) {
-    console.log("Error".red, error);
-    res.status(500).send({
-      error: error,
-    });
-  }
-});
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-// print stacktrace on error
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render("error", {
-    message: err.message,
-    error: err,
-  });
-});
 
 app.listen(port, () =>
   console.log(`Demo App Client listening on port ${port}!`)
